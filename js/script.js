acea8611885a4e3e0ef4dc7e210a3f68c29b3b1b
Olga Klimetyonok <olgaklimetenok@gmail.com>
2023-03-24 20:36:02 +0300

    feat: create calculator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//slider
+
 new Swiper('.hero__slider', {
     slidesPerView: 2,
     spaceBetween: 10,
@@ -17,4 +19,32 @@ new Swiper('.hero__slider', {
             spaceBetween: 8,
         }
     }
+});
+
+//calculator
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-square');
+const totalPrice = document.querySelector('.js-total-price');
+const calcResultWrapper = document.querySelector('.js-result-wrapper');
+
+const tariff = {
+    economy: 550,
+    comfort: 1400,
+    premium: 2700,
+}
+
+calcForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+        const square = calcForm.width.value * calcForm.length.value;
+        const price = square * tariff[calcForm.tariff.value];
+
+        calcResultWrapper.style.display = 'block';
+        totalSquare.textContent = `${square} кв м`;
+        totalPrice.textContent = `${price} руб`;
+    }
 })
+
+
